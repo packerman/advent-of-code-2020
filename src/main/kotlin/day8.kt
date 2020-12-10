707f31d7363866ceba1df15997028877d374764c
Piotr Ackermann <pioacker@gmail.com>
2020-12-10 18:39:37 +0100

    Day 8 (Part Two)

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -8,6 +8,8 @@ fun main() {
 
     println(Day8.part1Answer(program))
 
+    println(Day8.part2Answer(program))
+
 }
 
 object Day8 {
@@ -38,16 +40,36 @@ object Day8 {
             .toTypedArray()
 
     fun part1Answer(program: Array<Instruction>): Int {
-        val simulator = Simulator(program)
+        val emulator = Emulator(program)
         val executed = mutableSetOf<Int>()
-        while (simulator.next !in executed) {
-            executed.add(simulator.next)
-            simulator.executeNext()
+        while (emulator.next !in executed) {
+            executed.add(emulator.next)
+            emulator.executeNext()
         }
-        return simulator.acc
+        return emulator.acc
     }
 
-    class Simulator(private val program: Array<Instruction>) {
+    fun part2Answer(program: Array<Instruction>): Int {
+        fun tryWithChangedOperation(changed: Int): Int? {
+            val emulator = EmulatorWithChangedOperation(program, changed)
+            val executed = mutableSetOf<Int>()
+            while (!emulator.terminated) {
+                if (emulator.next in executed) {
+                    return null
+                }
+                executed.add(emulator.next)
+                emulator.executeNext()
+            }
+            return emulator.acc
+        }
+        return program.indices
+            .asSequence()
+            .filter { program[it].operation == Operation.nop || program[it].operation == Operation.jmp }
+            .map { tryWithChangedOperation(it) }
+            .first { it != null } ?: error("No such operation")
+    }
+
+    class Emulator(private val program: Array<Instruction>) {
 
         private var _next = 0
         private var _acc = 0
@@ -60,6 +82,49 @@ object Day8 {
 
         fun executeNext() {
             val instruction = program[_next]
+            when (instruction.operation) {
+                Operation.nop -> _next++
+                Operation.acc -> {
+                    _acc += instruction.argument
+                    _next++
+                }
+                Operation.jmp -> _next += instruction.argument
+            }
+        }
+    }
+
+    class EmulatorWithChangedOperation(
+        private val program: Array<Instruction>,
+        private val changedIndex: Int
+    ) {
+
+        private var _next = 0
+        private var _acc = 0
+
+        val next: Int
+            get() = _next
+
+        val acc: Int
+            get() = _acc
+
+        val terminated: Boolean
+            get() = _next >= program.size
+
+        fun executeNext() {
+            if (!terminated) {
+                val instruction = program[_next]
+                val changedInstruction = if (_next == changedIndex) {
+                    when (instruction.operation) {
+                        Operation.nop -> instruction.copy(operation = Operation.jmp)
+                        Operation.jmp -> instruction.copy(operation = Operation.nop)
+                        else -> instruction
+                    }
+                } else instruction
+                executeInstruction(changedInstruction)
+            }
+        }
+
+        private fun executeInstruction(instruction: Instruction) {
             when (instruction.operation) {
                 Operation.nop -> _next++
                 Operation.acc -> {
